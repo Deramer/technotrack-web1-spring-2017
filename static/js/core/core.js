547d641393801a880da94648dd69db89abac1c4a
Deramer <arseniy.nestyuk@gmail.com>
2017-04-03 20:34:33 +0300

    Footer work's correction.
    Or just crutch, as someone might put it.

--- static/js/core/core.js
+++ static/js/core/core.js
@@ -1,5 +1,7 @@
 $( document ).ready(function () {
 	$( '#header-mobile-icon' ).click(mobile_menu)
+	correct_footer_position()
+	$( window ).resize(correct_footer_position)
 })
 
 function mobile_menu() {
@@ -8,5 +10,14 @@ function mobile_menu() {
 		$li.removeClass( 'header-show' )
 	} else {
 		$li.addClass( 'header-show' )
+	}
+}
+
+function correct_footer_position() {
+	dif = $( '#sidebar' ).height() - $( '#content' ).height()
+	if (dif > 0) {
+		$( '#footer' ).css('margin-top', dif.toString() + 'px')
+	} else {
+		$( '#footer' ).css('margin-top', '0px')
 	}
 }
