006f2e12e3f358c6bde098174224b3719e4ac78b
Deramer <arseniy.nestyuk@gmail.com>
2017-04-04 15:38:31 +0300

    Sidebar filled without updates.

--- core/templatetags/core_extras.py
+++ core/templatetags/core_extras.py
@@ -1,6 +1,10 @@
 from django import template
 
+from blog.models import Post
+
+
 register = template.Library()
+
 
 @register.simple_tag(takes_context=True)
 def produce_get(context, **kwargs):
@@ -9,12 +13,14 @@ def produce_get(context, **kwargs):
         get[key] = value
     return '?' + get.urlencode()
 
+
 @register.simple_tag(takes_context=True)
 def produce_get_update(context, **kwargs):
     get = context['request'].GET.copy()
     for key, value in kwargs.items():
         get.appendlist(key, value)
     return '?' + get.urlencode()
+
 
 @register.simple_tag(takes_context=True)
 def produce_get_remove(context, **kwargs):
@@ -25,6 +31,12 @@ def produce_get_remove(context, **kwargs):
         get.setlist(key, key_list)
     return '?' + get.urlencode()
 
+
 @register.filter
 def querydict_get_list(querydict, key):
     return querydict.getlist(key)
+
+
+@register.inclusion_tag('core/sidebar.html')
+def fill_sidebar():
+    return {'list': Post.objects.order_by('-created_at')[:9]}
